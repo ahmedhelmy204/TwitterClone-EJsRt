37e56c0373adbe05ccb953d9f7c9f3ec34978af0
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2023-02-17 16:34:08 +0100

    frontend structure and install material-ui and material-ui-icons

---
+++ twitter.web/src/Sidebar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button -> Tweet */}
+        </div>
+    );
+}
+
+export default Sidebar;
